93873a108f3ca7bb842d1762813f6ebfdba4469a
GitHub Action <action@github.com>
2022-09-01 19:01:25 +0800

    baseActivity

---
+++ src/main/kotlin/other/activity/baseActivity.kt
@@ -0,0 +1,28 @@
+package other.activity
+
+
+fun baseActivity(
+  applicationPackage: String?,
+  activityClass: String,
+  layoutName: String,
+  packageName: String
+) = """
+package ${packageName}
+
+import android.os.Bundle
+import ${applicationPackage}.R
+import io.github.ChinaVolvocars.common.ui.activity.BaseActivity
+
+class ${activityClass}Activity : BaseActivity() {
+
+  override fun getContentViewLayoutID(): Int {
+    return R.layout.${layoutName}
+  }
+  
+  override fun initViewsAndEvents(savedInstanceState: Bundle?) {
+    title = ${layoutName}
+  }
+
+}
+   
+"""
