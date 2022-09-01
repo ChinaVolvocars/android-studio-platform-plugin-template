0871ed5c11dbd957664dae76c13688c9a5ddda9a
GitHub Action <action@github.com>
2022-09-01 11:01:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/chinavolvocars/androidstudioplatformplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chinavolvocars/androidstudioplatformplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chinavolvocars.androidstudioplatformplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chinavolvocars.androidstudioplatformplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/chinavolvocars/androidstudioplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chinavolvocars/androidstudioplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chinavolvocars.androidstudioplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chinavolvocars.androidstudioplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
