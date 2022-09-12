04b70cc2e757adb942255bbb49034e03d8ed7255
Atlantis <AtlantisSpeed@gmail.com>
2022-09-13 07:44:32 +0800

    WizardTemplateProvider

--- src/main/kotlin/other/SamplePluginTemplateProviderImpl.kt
+++ src/main/kotlin/other/SamplePluginTemplateProviderImpl.kt
@@ -4,15 +4,17 @@ import com.android.tools.idea.wizard.template.Template
 import com.android.tools.idea.wizard.template.WizardTemplateProvider
 import other.BaseListActivity.baseListActivityTemplate
 import other.BaseLoadListActivity.baseLoadListActivityTemplate
+import other.BaseLoadMoreActivity.baseLoadMoreActivityTemplate
 import other.activity.baseActivityTemplate
 import other.mvvm.activity.mvvmActivityTemplate
 
 class SamplePluginTemplateProviderImpl : WizardTemplateProvider() {
 
   override fun getTemplates(): List<Template> = listOf(
-      mvvmActivityTemplate,
-      baseActivityTemplate,
-      baseListActivityTemplate,
-      baseLoadListActivityTemplate,
+    mvvmActivityTemplate,
+    baseActivityTemplate,
+    baseListActivityTemplate,
+    baseLoadListActivityTemplate,
+    baseLoadMoreActivityTemplate,
   )
 }
