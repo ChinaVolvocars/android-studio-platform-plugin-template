76902cfb02488bd2e7d7322a35e681cde544753f
Atlantis <AtlantisSpeed@gmail.com>
2022-09-14 21:02:27 +0800

    baseFragment

---
+++ src/main/kotlin/fragment/baseFragment.kt
@@ -0,0 +1,26 @@
+package fragment
+
+
+fun baseFragment(
+  applicationPackage: String?,
+  fragmentClass: String,
+  layoutName: String,
+  packageName: String
+) = """
+package ${packageName}
+
+import android.os.Bundle
+import android.view.View
+import io.github.ChinaVolvocars.common.ui.fragment.BaseFragment
+
+class ${activityClass}Fragment : BaseFragment() {
+  override fun getLayoutId(): Int {
+    return R.layout.${layoutName}
+  }
+
+  override fun initViewsAndEvents(root: View?, savedInstanceState: Bundle?) {
+    
+  }
+}
+   
+"""
