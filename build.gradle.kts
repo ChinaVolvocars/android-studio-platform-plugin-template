b17080b5b938bf9d793f6b6fb6179cd748fc1219
Atlantis <atlantisspeed@gmail.com>
2023-06-13 10:37:46 +0800

    运行环境版本修改

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
   // Java support
   id("java")
   // Kotlin support
-  id("org.jetbrains.kotlin.jvm") version "1.7.10"
+  id("org.jetbrains.kotlin.jvm") version "1.8.21"
   // Gradle IntelliJ Plugin
-  id("org.jetbrains.intellij") version "1.8.0"
+  id("org.jetbrains.intellij") version "1.13.3"
   // Gradle Changelog Plugin
   id("org.jetbrains.changelog") version "1.3.1"
   // Gradle Qodana Plugin
@@ -28,7 +28,7 @@ repositories {
 // Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
 kotlin {
   jvmToolchain {
-    languageVersion.set(JavaLanguageVersion.of(11))
+    languageVersion.set(JavaLanguageVersion.of(17))
   }
 }
 
